b4fe6f41f8de9968cd74fc81d0690b864b95fb60
Anurag Affection <anuragaffection07@gmail.com>
2023-07-04 11:13:17 +0530

    Pattern Matching

--- ChapterNotes/M2DSA/ch1PatternMatch.js
+++ ChapterNotes/M2DSA/ch1PatternMatch.js
@@ -37,7 +37,7 @@ console.log(result);
 //  03.
 //  Literal Matching 
 //  regex is case sensitive 
-console.log("Literal Matching ");
+console.log("03. Literal Matching ");
 
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/;
@@ -58,7 +58,7 @@ console.log(rWadloRegexCapital); // false
 //  pipe | is used as or in regex
 //  will match any of thses four words 
 
-let petStr = "James has a pet cat.";
+let petStr = "04. James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/;
 let petResult = petRegex.test(petStr);
 
@@ -72,7 +72,7 @@ console.log(petResult); // true
 //  flag - i
 //  i - ignore case
 //  flag added last to last of regex
-console.log("how to overcome case sensitivity in regex");
+console.log("05. how to overcome case sensitivity in regex");
 
 let fccString = "FreeCodeCamp";
 
@@ -93,7 +93,7 @@ console.log(fccResult1);
 //  06. 
 //  .match()
 //  return string in array format 
-console.log("Extract Matches");
+console.log("06. Extract Matches");
 
 let extString = "Extract the word 'coding' from this stirng .";
 let codeRegex = /coding/;
@@ -108,7 +108,7 @@ console.log(codeResult);
 // 07.
 // i - ignore case - will ignore case sensitivity
 // g - global find - will found all the matches 
-console.log("find more than the first match");
+console.log("07. find more than the first match");
 
 let testStr = "Repeat, Repeat, Repeat ";
 let testRegex = /Repeat/;
@@ -127,7 +127,7 @@ console.log(twinResult);
 //  08.
 //  . (period or dot ) 
 //  at . (dot) there will be any character 
-console.log("Match anything with wildcard period ");
+console.log("08. Match anything with wildcard period ");
 
 let humStr = "I'll sing hum Hum Humm song";
 let hugStr = "Bear hug , Dear Hug, Hugges ";
@@ -144,7 +144,8 @@ console.log(hugResult);
 
 
 // 09.
-console.log('Exploring dot or period ');
+// . - any charcter at the place of dot 
+console.log('09. Exploring dot or period ');
 let exampleStr = "Let's have fun & run with regular expressions.";
 
 let exampleRegex = /.un/;
@@ -160,11 +161,151 @@ console.log(exampleResult1);
 
 
 
-// 10
-// 
-console.log("Match single character with multiple possibilities");
 
-let quotStr = "Beware of bugs "
+// 10
+// any character [] - from big bracket;
+console.log("10. Match single character with multiple possibilities");
+
+let quotStr = "Beware of bugs in the above code; I have only proved it correctly";
+
+let vowelRegex = /[aeious]/ig;
+let bgRegex = /b[aiu]g/;
+
+console.log(quotStr.match(vowelRegex));
+console.log(quotStr.match(bgRegex));
+
+
+
+
+
+// 11.
+// range of letters 
+console.log("11. Match Letters of the Alphabet ");
+
+let alphabetString = 'The quick brown fox jumps over a lazy dog.';
+let alphaRegex = /[a-z]/ig;
+console.log(alphabetString.match(alphaRegex));
+
+
+
+
+
+
+// 12.
+// range of letters
+// range of numbers
+console.log("12.Match number & letters of alphabet ");
+
+let mixString = "Bluebery 3.141429876544s are delicious.";
+
+let mixRegex = /[0-9a-z]/ig;
+let mixRegex1 = /[0-4a-e]/ig;
+
+console.log(mixString.match(mixRegex));
+console.log(mixString.match(mixRegex1));
+
+
+
+
+
+
+// 13
+// ^ - negate 
+console.log("13. Match single character not specified ");
+
+let miceStr = "3 Blind Mice.";
+
+let miceRegex = /[0-9aeiou]/ig;
+let miceRegex1 = /[^0-9aeiou]/ig;
+
+console.log(miceStr.match(miceRegex));
+console.log(miceStr.match(miceRegex1));
+
+
+
+
+
+
+// 14
+// + is used to match when same word is occuring consectively 
+console.log("14. Match Character that occur one more time");
+
+let diffString = "Mississippsi";
+let diffRegex = /s+/ig;
+console.log(diffString.match(diffRegex));
+
+
+
+
+// 15
+// * returnig multiple match 
+// more advance of +
+console.log("15. Match character that occurs zero or more time ");
+
+let soccerStr = "gooooooooolll!";
+let feelStr = "gut feeling is gut go ggg feel";
+let moonStr = "Over the moon, on the moon";
+
+let sfmRegex = /go*/ig;
+
+console.log(soccerStr.match(sfmRegex));
+console.log(feelStr.match(sfmRegex));
+console.log(moonStr.match(sfmRegex));
+
+
+
+
+// 16
+console.log("16. Exploring more ");
+
+let chewStr = 'Aaaaaaaaaaaaaarrrrgh!';
+let chewRegex = /Aa*/;
+console.log(chewStr.match(chewRegex));
+
+
+
+
+// 17.
+// ? - 
+console.log('17. find character with lazy matching ');
+
+let titStr = "titanic";
+
+let titRegex = /t[a-z]*i/ig;
+let titRegex1 = /t[a-z]*?i/ig;
+
+console.log(titStr.match(titRegex));
+console.log(titStr.match(titRegex1));
+
+
+
+
+// 18.
+// . means any character are allowed 
+// . wildcard character 
+console.log("18.More on lazy matching ");
+
+let winStr  = "<h1> Winter is coming <... </h1>";
+
+let winRegex = /<.*>/;
+let winRegexQues = /<.*?>/;
+
+let winRegex1 = /<>/;
+let winRegex2 = /<.>/;
+let winRegex3 = /.*/;
+
+console.log(winStr.match(winRegex));
+console.log(winStr.match(winRegexQues));
+
+console.log(winStr.match(winRegex1));
+console.log(winStr.match(winRegex2));
+console.log(winStr.match(winRegex3));
+
+
+
+// 19.
+
+
 
 
 
