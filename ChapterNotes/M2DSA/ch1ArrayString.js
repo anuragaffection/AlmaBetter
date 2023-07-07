37423a55de4524f304a9e2cce6496b08e512a29c
Anurag Affection <anuragaffection07@gmail.com>
2023-07-07 07:48:17 +0530

    Array & String

--- ChapterNotes/M2DSA/ch1ArrayString.js
+++ ChapterNotes/M2DSA/ch1ArrayString.js
@@ -633,3 +633,101 @@ for ( let i in myarray){
 // 28.
 // 
 
+
+
+
+
+
+//
+// string
+// 
+
+
+
+// 01. 
+console.log("01. multi line string with + operator ")
+
+let str = "Hello this is going to be multi line statements " + 
+          "Using + operator " + 
+          "so yeah that all. ";
+
+        
+let str1 = 'Hello this is going to be multi line statements  \
+            Using  operator  \
+            so yeah that all.';
+
+console.log(str, str1);
+
+
+
+
+//02.
+console.log("02. String Method");
+
+/*
+
+charAt(): 
+        Returns the character at the specified index of a string.
+
+
+charCodeAt(): 
+            Returns the Unicode code point of the character at the specified index of a string.
+
+
+concat(): 
+         Concatenates two or more strings together.
+
+
+fromCharCode(): 
+              Converts a sequence of Unicode code points to a string.
+
+
+indexOf():
+          Returns the index of the first occurrence of a substring in a string.
+
+
+lastIndexOf(): 
+             Returns the index of the last occurrence of a substring in a string.
+
+
+length(): 
+        Returns the length of a string.
+
+
+match(): 
+        Returns an array of matches for a regular expression in a string.
+
+
+replace(): 
+         Replaces all occurrences of a substring in a string with another string.
+
+
+search(): 
+        Returns the index of the first occurrence of a regular expression in a string.
+
+
+slice(): 
+        Returns a new string that is a substring of the original string.
+
+
+split(): 
+        Splits a string into an array of substrings.
+
+
+substr(): 
+        Returns a substring of a string, starting at the specified index and ending at the specified length.
+
+
+toLowerCase(): 
+            Converts a string to lowercase.
+
+
+toUpperCase(): 
+            Converts a string to uppercase.
+
+
+*/
+
+
+// 03. 
+console.log("String () function in javascript ");
