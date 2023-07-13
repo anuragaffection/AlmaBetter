833378abbf7edf8b419048617232fcd18f15e1af
Anurag Affection <anuragaffection07@gmail.com>
2023-07-13 10:40:04 +0530

    Git & Github

---
+++ Extra/git.js
@@ -0,0 +1,94 @@
+/**
+ * 
+ *   01. What is Git & Github ?
+ *   --- 
+ * 
+ * 
+ * 
+ *   02. What is version control ?
+ *   --- ways to maintain & control the different or multiple version of application.
+ *   --- makes easy to collaborate
+ *   --- it maintain the previous version of application
+ *   --- keeps the track of changes in application
+ * 
+ * 
+ * 
+ *   03. Different types of Version Control System
+ *   --- Localized vc
+ *   --- Centralized vc 
+ *   --- Distributed vc 
+ * 
+ * 
+ * 
+ *   04. Localized version control 
+ *   --- everything is happening on local or my computers
+ *   --- changes of data loss is high, hardware crash, power failure
+ * 
+ * 
+ * 
+ *   05. Centralized version control 
+ *   --- here we have centralized server, or server to store version 
+ *   --- Data loss chance, is less compared to localized vc 
+ *   --- here, single failure, will affect many system 
+ * 
+ * 
+ * 
+ *   06. Distributed Version Control
+ *   --- No single point of failure of version
+ *   --- here, we have centralized server + each individual have copy of version
+ * 
+ * 
+ * 
+ *   07. Version control tool 
+ *   --- tool to manage version of applicaation
+ *   --- eg git
+ * 
+ * 
+ * 
+ *   08. Git 
+ *   --- It is a distributed version control system tool.
+ * 
+ * 
+ * 
+ *   09. Git Snapshot
+ *   --- State of the project or code at a specific moment or time
+ *   --- Every commit have new snapshot 
+ * 
+ * 
+ * 
+ *   10. Git states 
+ *   --- Modified - state when me make change to file 
+ *   --- Staged - state when git started to track the code 
+ *   --- Commited - file call to commit the changes
+ * 
+ * 
+ * 
+ *   11. Git Local 
+ *   --- Installing git 
+ *   --- Configuring git 
+ *   --- Create git project 
+ *   --- Git Operations 
+ * 
+ * 
+ *   12. Installing git 
+ *   --- https://git-scm.com/downloads
+ *   --- to check whether git is installed or not = git --version
+ *   --- to check whether git is installed or not = git version
+ * 
+ * 
+ * 
+ *   13. Configuring git 
+ *   --- to login or signup, who is the users 
+ *   --- 
+ * 
+ * 
+ * 
+ *   14. Some basic windows command 
+ *   --- to move to next folder = cd <folderName>
+ *   --- to go back to previous folder = cd ..
+ * 
+ *   
+ * 
+ * 
+ * 
+*/
