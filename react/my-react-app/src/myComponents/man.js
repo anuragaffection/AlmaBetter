4284f60cb289c5d02523c2b88b20b69d65ca356c
Anurag Affection <anuragaffection07@gmail.com>
2023-08-05 07:32:49 +0530

    User defined Components

---
+++ react/my-react-app/src/myComponents/man.js
@@ -0,0 +1,14 @@
+// creating function based componets 
+
+
+import React,{Component} from "react";
+
+
+// here function name should starting by capital letters
+
+// here we are using named exports 
+export const Man = () => {
+    return <h1>MERN - JAVA - DSA - AWS </h1>;
+    
+}
+
