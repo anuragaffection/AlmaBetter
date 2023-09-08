6474961a155b334654bc2ed2ef37fd51a7afff39
Anurag Affection <anuragaffection07@gmail.com>
2023-09-08 15:48:55 +0530

    String

---
+++ WebDev/M2DSA/01basic/ch12String.js
@@ -0,0 +1,118 @@
+//
+// string
+// 
+
+
+
+/*
+   *  Introduction to string - (in other module )
+   *  -- what is a string 
+   *  -- why is a string data required 
+   *  -- string declration 
+   *  -- string operations 
+   *  -- working with string 
+
+*/
+
+
+
+
+/*
+*  01. String introduction 
+*  02. multi line string with + operator --- important 
+*  03. String method , list 
+*  04. String () - function in js
+*/
+
+
+
+// 01. 
+console.log("01. multi line string with + operator ")
+
+let str = "Hello this is going to be multi line statements " + 
+          "Using + operator " + 
+          "so yeah that all. ";
+
+        
+let str1 = 'Hello this is going to be multi line statements  \
+            Using  operator  \
+            so yeah that all.';
+
+console.log(str, str1);
+
+
+
+//02.
+
+
+
+//03.
+console.log("02. String Method");
+
+/*
+
+charAt(): 
+        Returns the character at the specified index of a string.
+
+
+charCodeAt(): 
+            Returns the Unicode code point of the character at the specified index of a string.
+
+
+concat(): 
+         Concatenates two or more strings together.
+
+
+fromCharCode(): 
+              Converts a sequence of Unicode code points to a string.
+
+
+indexOf():
+          Returns the index of the first occurrence of a substring in a string.
+
+
+lastIndexOf(): 
+             Returns the index of the last occurrence of a substring in a string.
+
+
+length(): 
+        Returns the length of a string.
+
+
+match(): 
+        Returns an array of matches for a regular expression in a string.
+
+
+replace(): 
+         Replaces all occurrences of a substring in a string with another string.
+
+
+search(): 
+        Returns the index of the first occurrence of a regular expression in a string.
+
+
+slice(): 
+        Returns a new string that is a substring of the original string.
+
+
+split(): 
+        Splits a string into an array of substrings.
+
+
+substr(): 
+        Returns a substring of a string, starting at the specified index and ending at the specified length.
+
+
+toLowerCase(): 
+            Converts a string to lowercase.
+
+
+toUpperCase(): 
+            Converts a string to uppercase.
+
+
+*/
+
+
+// 04. 
+console.log("String () function in javascript ");
