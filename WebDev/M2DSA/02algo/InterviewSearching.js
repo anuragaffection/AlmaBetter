54ef8181524c83b561122c40d1d30b221ece2c63
Anurag Affection <anuragaffection07@gmail.com>
2023-09-09 10:41:04 +0530

    Searching Interview Questions

---
+++ WebDev/M2DSA/02algo/InterviewSearching.js
@@ -0,0 +1,18 @@
+
+// Searching Questions
+
+// 2.1. How would you find the index of a particular item in an array in JavaScript?
+
+// 2.2. How would you check if a particular value exists in an array or not?
+
+// 2.3. Could you explain the difference between indexOf() and findIndex() methods in JavaScript?
+
+// 2.4. Explain the concept of Binary Search. What are its time and space complexities?
+
+// 2.5. How would you find the first non-repeating character in a string using JavaScript?
+
+// 2.6. Describe how you would implement a depth-first search on a tree data structure in JavaScript.
+
+// 2.7. Explain how to use filter() and find() methods in JavaScript to search for elements in an array.
+
+// 2.8. How would you implement a function to perform a case-insensitive search for a substring within a string?
