8325c97b38fc8b5709bea2e397028ceea6c8ca09
Anurag Affection <anuragaffection07@gmail.com>
2023-09-14 08:39:07 +0530

    components page

--- Projects/joke-generator/app/components/Navbar.jsx
+++ Projects/joke-generator/app/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
     return (
@@ -13,18 +14,20 @@ export default function Navbar() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>
-                    
+                 
                     <span class="ml-3 text-xl">Joke Generator </span>
+
                 </a>
+
 
                 <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link href='/' class="mr-5 hover:text-white">Home </Link>
+                    <Link href='/joke' class="mr-5 hover:text-white">Jokes  </Link>
                     <Link href='/about' class="mr-5 hover:text-white">About </Link>
-                    <Link href='/jokes' class="mr-5 hover:text-white">Jokes </Link>
                     <Link href='/contact' class="mr-5 hover:text-white">Contact </Link>
                 </nav>
 
-               
+
 
             </div>
         </header>
