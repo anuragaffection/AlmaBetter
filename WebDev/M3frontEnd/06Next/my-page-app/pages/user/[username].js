16767b5629a4d4f631d4b885e814f33c7f18ba92
Anurag Affection <anuragaffection07@gmail.com>
2023-09-19 15:22:39 +0530

    till dynamic folder

---
+++ WebDev/M3frontEnd/06Next/my-page-app/pages/user/[username].js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import {useRouter} from 'next/router';
+
+
+export default function username() {
+
+    const router = useRouter();  // this will provide lots of thing about, check console 
+    
+    // console.log('router = ', router);
+
+    /**
+     * some important thing that useRotuer() provide 
+     * 
+     * -- query
+     * -- asPath
+     * -- route 
+     */
+
+
+  return (
+
+    <div>
+        <h1>{router?.query.username} , Welcome to Next App </h1>
+
+        {
+          /* 
+                why we used  -- router?  -- 
+                because if user not found then it's going to be undefined
+
+                
+          */
+        }
+
+
+    </div>
+
+
+
+    
+  )
+}
