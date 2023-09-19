a8e5c8d1fdeb24e9d489dcc4c6d3a0578847b397
Anurag Affection <anuragaffection07@gmail.com>
2023-09-19 13:50:06 +0530

    till Nested Routing

---
+++ WebDev/M3frontEnd/06Next/my-page-app/pages/about.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>This is the about Page </h1>
+        </div>
+    )
+
+}
+
+
+export default AboutPage;
---
+++ WebDev/M3frontEnd/06Next/my-page-app/pages/user/anu.js
@@ -0,0 +1,9 @@
+
+
+export default function () {
+
+  return (
+
+    <div> Anurag is my Name. </div>
+  );
+}
