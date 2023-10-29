9bed9b47d8f700d02816a0e7764510f8caa85088
Anurag Affection <anuragaffection07@gmail.com>
2023-10-29 20:20:59 +0530

    updated

---
+++ WebDev/m04backend/09Extra/CWH/MongoDB.js
@@ -0,0 +1,13 @@
+
+db.collection.insertOne(
+    {
+        item: "canvas",
+        gty: 100,
+        tags: ["cotton"],
+        size: {
+            h: 28,
+            w: 35.5,
+            uom: "cm",
+        }
+    }
+)
