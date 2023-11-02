1f3273fd1662a657afcd8f060227b3e69adf7e81
Anurag Affection <anuragaffection07@gmail.com>
2023-11-02 18:38:46 +0530

    Updated

--- JavaDSA/javadsa02/Ch16String/P147LargestString.java
+++ JavaDSA/javadsa02/Ch16String/P147LargestString.java
@@ -1,5 +1,6 @@
 package JavaDSA.javadsa02.Ch16String;
 
+
 /**
  * -- problem statements 
  * 
@@ -16,5 +17,29 @@ package JavaDSA.javadsa02.Ch16String;
 
 
 public class P147LargestString {
+
+    public static void main (String args[]){
+        String fruits [] = {"apple", "Apple", "mango", "Mango"};
+        String largest = fruits[0];
+        String largestIgnoreCase = fruits[0];
+
+        for (int i = 1; i < fruits.length; i++){
+            if (largest.compareTo(fruits[i]) < 0){
+                largest = fruits[i];
+            }
+
+            if (largest.compareToIgnoreCase(fruits[i]) < 0){
+                largestIgnoreCase = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+        System.out.println(largestIgnoreCase);
+    }
     
 }
+
+// a & A both have different value by default 
+// here largest is getting decides by value of first letter of word
+
+// compareToIgnoreCase() treats a and A both as same 
