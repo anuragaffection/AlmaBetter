f19fc2b19ef44623cd31c7b63d52e31f398e6015
Anurag Affection <anuragaffection07@gmail.com>
2023-11-25 20:41:49 +0530

    updated

--- WebDev/m04backend/04nodejs/Ch42NodeBasics/Ch42t01Server.js
+++ WebDev/m04backend/04nodejs/Ch42NodeBasics/Ch42t01Server.js
@@ -1,11 +1,16 @@
-const http = require('http'); // importing  http module 
+const http = require('http'); 
+// importing  http module for server 
 
-const  server = http.createServer( (req, res) => { // creating http server 
+
+const  server = http.createServer( (req, res) => { 
+    // creating http server 
     // handling incoming requests [req, res ] 
 
-    res.end("<h1> Hello World </h1>") // sending response to  client 
+    res.end("<h1> Hello World </h1>") 
+    // sending response to  client 
 })
 
-server.listen( 3001, () => { // starting the server 
+server.listen( 3001, () => { 
+    // starting the server 
     console.log("server is running");
 })
--- WebDev/m04backend/08Extra/CwhNodeJS/L10serverExp.js
+++ WebDev/m04backend/08Extra/CwhNodeJS/L10serverExp.js
@@ -3,18 +3,44 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
+
+    // just seding response 
     res.send('Hello World from Home page ');
 });
 
+
+
+
 app.get('/about', (req, res) => {
+
+    // just sending response 
     res.send('Welcome to  About page. We are on mission to Moon ');
 });
+
+
+
+
+app.get('/products', function (req, res) {
+
+    // we are using & getting request from web 
+    const productId = req.params.id
+
+    // send response to web 
+    res.send(`Product ID: ${productId}`)
+})
+
+
+
 
 app.get('/products/:id', function (req, res) {
     const productId = req.params.id
     res.send(`Product ID: ${productId}`)
 })
+
+
+
 
 
 const users = [
@@ -23,9 +49,11 @@ const users = [
     { id: 3, name: 'Charlie' }
 ];
 
+
 app.get('/users', (req, res) => {
     res.json(users);
 });
+
 
 
 
@@ -38,6 +66,8 @@ app.post('/users', (req, res) => {
 });
 
 
+
+
 // app.use(function (req, res, next) {
 //     console.log(`${req.method} ${req.url}`)
 //     next()
@@ -46,13 +76,20 @@ app.post('/users', (req, res) => {
 
 
 
+// running the serve 
 app.listen(3000, () => {
     console.log("Server started on port 3000 ");
 })
-
 // http://localhost:3000/
+
+
+
+
+
+
 
 
 
 // task
 // res.json() vs res.send()
+// res.end() vs res.send()
