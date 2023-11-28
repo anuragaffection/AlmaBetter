91e735f47e35e9172eaec7d73d71059594859ccb
Anurag Affection <anuragaffection07@gmail.com>
2023-11-28 15:33:22 +0530

    updated

--- Projects/backend_project/server.js
+++ Projects/backend_project/server.js
@@ -6,13 +6,12 @@ const dotenv = require('dotenv').config();
 
 connectDb();
 const app=express();
-
 const port = process.env.PORT;
+
 app.use(express.json())
 app.use("/api/contacts",require("./routes/contactRoutes"))
 app.use("/api/users",require("./routes/userRoutes"))
 app.use(errorHandler)
-
 
 
 app.listen(port,()=>{
