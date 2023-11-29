89d576a384576bb290fbfaf879df7e608fc04568
Anurag Affection <anuragaffection07@gmail.com>
2023-11-29 13:35:28 +0530

    updated

--- Projects/blog_mern/backend/routes/user.js
+++ Projects/blog_mern/backend/routes/user.js
@@ -1,6 +1,11 @@
-import Express  from "express";
+import Express from "express";
+import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
+import {User} from "../server.js";
+
 
 const router = Express.Router();
+
 
 router.get('/', (req, res) => {
     res.json({
@@ -11,21 +16,90 @@ router.get('/', (req, res) => {
 })
 
 
-router.get('/post', (req, res) => {
-    res.json({
-        success: true,
-        message: 'We are in post route',
-        suman: 'Web Dev by Suman'
+
+
+router.post('/register', async (req, res) => {
+
+    // destructuring , after  recieving from req.body 
+    const { name, email, password } = req.body;
+
+
+    let user = await User.findOne({ email });
+    if (user) return res.status(404).json({
+        success: false,
+        message: "user already exist",
     })
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // updating password  with hashpassword 
+    user = await User.create({
+        name,
+        email,
+        password: hashPassword,
+    });
+
+    const token = Jwt.sign({ _id: user._id }, '!@#$%^&*()+')
+
+    res.status(201).cookie("token", token, {
+        httpOnly: true,
+        maxAge: 10 * 60 * 1000
+    }).json({
+        success: true,
+        message: "user Registered successfully",
+        data: user
+    })
+
 })
 
 
-router.get('/about', (req, res) => {
-    res.json({
+
+
+
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({
+        success: false,
+        message: "user do not exist",
+    });
+
+    // user.password = comming from db 
+    // password = comming from frontend website 
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Credential"
+        })
+    }
+
+    const token = Jwt.sign({ _id: user._id }, '!@#$%^&*()+')
+
+    res.status(201).cookie("token", token, {
+        httpOnly: true,
+        maxAge: 10 * 60 * 1000
+    }).json({
         success: true,
-        message: 'We are in about route',
-        suman: 'Web Dev by Suman'
+        message: `${user.name} logined successfully`,
+        data: user
+    })
+
+})
+
+
+router.get('/logout', (req, res) => {
+    res.status(200).cookie("token", "", {
+        expires: new Date(Date.now())
+    }).json({
+        success : true,
+        message : "logout successfully"
     })
 })
+
 
 export default router;
--- Projects/blog_mern/backend/server.js
+++ Projects/blog_mern/backend/server.js
@@ -1,8 +1,5 @@
 import Express from "express";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import Jwt from "jsonwebtoken";
-
 import userRouter from "./routes/user.js";
 
 
@@ -41,108 +38,13 @@ const userSchema = new mongoose.Schema({
 
 // creating model, why ?
 // behind the scene it works as class constructors 
-const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
 
 
-app.use('/', userRouter);
-
-
-
-app.post('/api/users/register', async (req, res) => {
-
-    // destructuring , after  recieving from req.body 
-    const { name, email, password } = req.body;
-
-
-    let user = await User.findOne({ email });
-    if (user) return res.status(404).json({
-        success: false,
-        message: "user already exist",
-    })
-
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    // updating password  with hashpassword 
-    user = await User.create({
-        name,
-        email,
-        password: hashPassword,
-    });
-
-    const token = Jwt.sign({ _id: user._id }, '!@#$%^&*()+')
-
-    res.status(201).cookie("token", token, {
-        httpOnly: true,
-        maxAge: 10 * 60 * 1000
-    }).json({
-        success: true,
-        message: "user Registered successfully",
-        data: user
-    })
-
-
-    // storing in single variable, the whole json 
-    // const user = await User.create({
-    //     name, email, password
-    // })
-
-
-
-
-    /*
-
-    //  returning or responses send 
-
-    res.json( {
-        success : true, 
-        message : 'User registered successfully',
-        suman : 'Web Dev by Suman',
-        user 
-    })
-
-    */
-
-})
-
-
-
-app.post('/api/users/login', async (req, res) => {
-
-    const { email, password } = req.body;
-
-    let user = await User.findOne({ email });
-
-    if (!user) return res.status(400).json({
-        success: false,
-        message: "user do not exist",
-    });
-
-    // user.password = comming from db 
-    // password = comming from frontend website 
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-        return res.status(400).json({
-            success: false,
-            message: "Invalid Credential"
-        })
-    }
-
-    const token = Jwt.sign({ _id: user._id }, '!@#$%^&*()+')
-
-    res.status(201).cookie("token", token, {
-        httpOnly: true,
-        maxAge: 10 * 60 * 1000
-    }).json({
-        success: true,
-        message: `${user.name} logined successfully`,
-        data: user
-    })
-
-})
-
-
+app.use('/api/users', userRouter);
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
 
