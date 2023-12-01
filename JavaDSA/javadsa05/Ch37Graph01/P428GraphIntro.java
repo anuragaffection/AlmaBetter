bf0c417e8c15a84d1d1effc76e168e4f898d191d
Anurag Affection <anuragaffection07@gmail.com>
2023-12-01 20:16:22 +0530

    updated

---
+++ JavaDSA/javadsa05/Ch37Graph01/P428GraphIntro.java
@@ -0,0 +1,26 @@
+
+// import java.util.*;
+// package JavaDSA.javadsa05.Ch37Graph01;
+
+class P428GraphIntro {
+
+    public static void main (String args[]){
+        System.out.println("Hello Graph ");
+    }
+}
+
+
+/*
+ * 
+ * Graph 
+ * -- collection of nodes 
+ * -- network of nodes 
+ * 
+ * Vertex 
+ * -- node of graph 
+ * 
+ * Edge
+ * -- line joining nodes 
+ * 
+ * 
+ */
---
+++ JavaDSA/javadsa05/Ch37Graph01/P429GraphTypes.java
@@ -0,0 +1,48 @@
+package JavaDSA.javadsa05.Ch37Graph01;
+
+public class P429GraphTypes {
+    
+    public static void main (String args []){
+        System.out.println("Types of Graph ");
+    }
+}
+
+
+/*
+ * 
+ * Edges 
+ * -- uni directional 
+ * -- bi directional or un directional 
+ * 
+ * Graph 
+ * -- directed graph 
+ * -- undirected graph 
+ * 
+ * Graph 
+ * -- Weighted graph 
+ * -- unWeighted graph 
+ * 
+ * Weight 
+ * -- value of edge 
+ * 
+ * 
+ * 
+*/
+
+
+/**
+ * 
+ * directed graph 
+ * -- having direction of edges
+ * 
+ * undirected graph 
+ * -- no direction 
+ * -- di direction 
+ * 
+ * Weighted graph 
+ * -- have value of edge 
+ * 
+ * unWeighted graph 
+ * -- without the value of edge 
+ * 
+ */
