2406bdf1eb97ed1ba97afa7dfac57adbb57f4a87
Anurag Affection <anuragaffection07@gmail.com>
2023-12-15 18:36:40 +0530

    updated

--- WebDev/m01javascript/01basic/ch14SkillMastery.js
+++
@@ -1,6 +0,0 @@
-/**
- *  14. Skill Mastery
-        -- https://javascript.plainenglish.io/what-are-javascript-programming-paradigms-3ef0f576dfdb
-        -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-
- */
