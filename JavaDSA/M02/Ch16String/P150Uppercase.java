1eaac0f1292b97515587bd2a84f23bbdf59d4bc3
Anurag Affection <anuragaffection07@gmail.com>
2023-12-16 00:56:36 +0530

    updated

--- JavaDSA/M02/Ch16String/P150Uppercase.java
+++ JavaDSA/M02/Ch16String/P150Uppercase.java
@@ -10,11 +10,48 @@
  * -- StringBuilder 
  * -- append()
  * -- Character 
+ * 
+ * -- ''  = char type in java 
+ * -- ""  = string type in java 
+ *  
  */
+
+
 
 public class P150Uppercase {
 
+    public static String convertFirstLetter(String str){
+
+        StringBuilder convertedStr = new StringBuilder("");
+
+        char veryFirstLetter = Character.toUpperCase(str.charAt(0));
+        convertedStr.append(veryFirstLetter);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' '){
+                convertedStr.append(str.charAt(i));
+                i++;
+                convertedStr.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else {
+                convertedStr.append(str.charAt(i));
+            }
+        }
+
+        return convertedStr.toString();
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Anurag Affection");
+        System.out.println("Problem Number 150 ");
+
+        String str = "hello world, i am anurag affection";
+        String result = convertFirstLetter(str);
+
+        System.out.println(result);
     }
 }
+
+// thank you 
+// all the very best 
+// link in description 
--- JavaDSA/M02/Ch16String/P151StringCompression.java
+++ JavaDSA/M02/Ch16String/P151StringCompression.java
@@ -1,17 +1,69 @@
-package JavaDSA.javadsa02.Ch16String;
 
 /*
  * -- #151 
  * -- String compression 
+ * -- length of compressed strig should be less than or equal to original one 
+ * 
+ * -- example. 
  * -- aaabbcccdd = a3b2c3d2
- * -- 
+ * -- abbcc = ab2c2 
+ * -- abcd = abcd 
+ * -- we have to compress string as shown in example 
  */
 
+ 
 /**
+ * -- time complexity = linear 
+ * -- space complexity = constant 
  * 
+ * -- Integer vs int 
+ * -- chaAt()
+ * -- toString()
+ * 
+ * -- we cannot convert  #int    to  #String  using toString()
+ * -- but we can convert #Integer to #String  using toString()
  */
 
  
 public class P151StringCompression {
+
+    public static String compression(String str){
+        String compressedStr = "";
+        
+        for (int i = 0; i < str.length(); i++){
+            Integer repeatLetters = 1 ;
+
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                repeatLetters++;
+                i++;
+            }
+
+            compressedStr += str.charAt(i);
+
+            if (repeatLetters > 1){
+                compressedStr += repeatLetters.toString();
+            }
+        }
+        return compressedStr;
+    }
     
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Anurag Affection. #Problem 151");
+
+        String str1 = "aaabbcccdd";
+        String str2 = "abcd";
+        String str3 = "abccddaaa";
+
+        String result1 = compression(str1);
+        String result2 = compression(str2);
+        String result3 = compression(str3);
+
+        System.out.println(result1); // a3b2c3d2
+        System.out.println(result2); // abcd 
+        System.out.println(result3);
+    }
 }
+
+// thank you 
+// all the very best 
