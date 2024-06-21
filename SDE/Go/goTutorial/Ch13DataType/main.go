90b9633a8a98d04f2cfd69dd46251d351ee6a52b
Anurag Affection <anuragaffection07@gmail.com>
2024-06-21 17:27:00 +0530

    updated

--- SDE/Go/goTutorial/Ch13DataType/main.go
+++ SDE/Go/goTutorial/Ch13DataType/main.go
@@ -1,35 +1,32 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
-func main () {
+func main() {
 
 	/*
-	   Data type is used to define the type of variable, 
-	   also it define the memory the variable is going to take 
-
-	   In go we have data types as 
-	   -- bool
-	   -- int 
-	   -- float32, float64
-	   -- string 
-	   -- complex128, complex256
-
-	   we  have some types like
-	   -- byte
-	   -- uint 
-
+		-- Data type is used to define the type of variable, also it define the memory the variable is going to take
+		-- types of variable in go
+		-- 1. bool
+		-- 2. int
+		-- 3. float , float32, float64
+		-- 4. string
+		-- 5. complex, complex64, complex128
+		-- 6. byte
+		-- 7. rune
+		-- 8. uint, uint8, uint16, uint32, uint64,
+		-- 9. uintptr
 	*/
 
 	fmt.Println("Data types in Go ")
 
-	var haveGf bool = false; 
-	var numberOfGf int = 1; 
-	var moneySpendOnGf float32 = 98765.2345;
+	var haveGf bool = false
+	var numberOfGf int = 1
+	var moneySpendOnGf float32 = 98765.2345
 	var nameOfGf string = "Natasha"
-	var expectationsOfGf complex128 = 2i + 56;
+	var expectationsOfGf complex128 = 2i + 56
 
 	fmt.Printf("Type of %t is %T \n", haveGf, haveGf)
 	fmt.Printf("Type of %d is %T \n", numberOfGf, numberOfGf)
@@ -38,5 +35,5 @@ func main () {
 	fmt.Printf("Type of %v is %T \n", expectationsOfGf, expectationsOfGf)
 }
 
-// thank you 
-// all the very best 
+// thank you
+// all the very best
